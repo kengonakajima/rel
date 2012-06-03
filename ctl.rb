bae128075203147fa64df1575febad5e8ba27fc5
kengo nakajima <kengo.nakajima@gmail.com>
2012-06-03 16:22:00 +0900

    up

--- ctl.rb
+++ ctl.rb
@@ -3,11 +3,13 @@
 require "rumino/rumino"
 require "common"
 
+INITD = "/etc/init.d"
+
 def main(argv)
   if argv.size != 2 then 
     eexit "Usage: ctl.rb ENVNAME start|stop"
   end
-  
+  cmd "#{INITD}/
 end
 
 #
